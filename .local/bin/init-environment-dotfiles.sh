1116b6c399ea6e9ff2c884f4f05fe6d2fdda0825
Skeptic Spriggan <skepticspriggan@protonmail.com>
2023-10-29 19:42:12 +0100

    feat: add dotfiles environment setup script

---
+++ .local/bin/init-environment-dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SESSION="dotfiles"
+
+if [[ -n "$(tmux ls | grep $SESSION)" ]]; then
+  tmux attach -t $SESSION
+  exit 1
+fi
+
+SESSION_PATH="$HOME"
+tmux -2 new-session -d -s $SESSION -c $SESSION_PATH 
+
+WINDOW="editor"
+
+tmux rename-window -t $SESSION:1 $WINDOW
+tmux send -t $SESSION:$WINDOW 'nvim .' ENTER
+
+WINDOW="commander"
+
+tmux new-window -n $WINDOW -c $SESSION_PATH
+
+tmux send -t $SESSION:$WINDOW.1 'git status'
+
+tmux attach -t $SESSION:2.1
+
