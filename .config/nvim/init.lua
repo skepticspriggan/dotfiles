953fdbf4eb42900556d5109b8b6edc432c2cf197
Skeptic Spriggan <skepticspriggan@protonmail.com>
2023-11-05 16:04:29 +0100

    feat: update harpoon navigation key bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -473,19 +473,19 @@ cmp.setup {
 
 vim.keymap.set('n', '<leader>m', require("harpoon.mark").add_file, { desc = '[H]arpoon [M]ark' })
 vim.keymap.set('n', '<leader>ht', require("harpoon.ui").toggle_quick_menu, { desc = '[H]arpoon [T]oggle Quick Menu' })
-vim.keymap.set('n', '<leader>1',
+vim.keymap.set('n', '<C-a>', --'<leader>1'
   function() require("harpoon.ui").nav_file(1) end,
   { desc = '[H]arpoon navigate to mark [1]' }
 )
-vim.keymap.set('n', '<leader>2',
+vim.keymap.set('n', '<C-s>', --'<leader>2'
   function() require("harpoon.ui").nav_file(2) end,
   { desc = '[H]arpoon navigate to mark [2]' }
 )
-vim.keymap.set('n', '<leader>3',
+vim.keymap.set('n', '<C-d>', --'<leader>3'
   function() require("harpoon.ui").nav_file(3) end,
   { desc = '[H]arpoon navigate to mark [3]' }
 )
-vim.keymap.set('n', '<leader>4',
+vim.keymap.set('n', '<C-f>', --'<leader>4'
   function() require("harpoon.ui").nav_file(4) end,
   { desc = '[H]arpoon navigate to mark [4]' }
 )
