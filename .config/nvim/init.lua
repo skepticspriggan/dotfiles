71037531edee98f11bf08e133d98682b24fe92af
Skeptic Spriggan <skepticspriggan@protonmail.com>
2023-11-13 21:58:36 +0100

    feat: move blankline plugin configuration to separate file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -74,18 +74,6 @@ require('lazy').setup({
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',  opts = {} },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    main = "ibl",
-    opts = {
-      indent = { char = '┊' },
-      whitespace = { remove_blankline_trail = false }
-    },
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
