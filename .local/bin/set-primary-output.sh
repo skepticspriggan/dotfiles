b0487ae6ac9825e881ac6344d94975ae31c984c6
Skeptic Spriggan <skepticspriggan@protonmail.com>
2023-11-16 20:51:01 +0100

    correct: fix cannot set primary monitor issue

--- .local/bin/set-primary-output.sh
+++ .local/bin/set-primary-output.sh
@@ -6,7 +6,7 @@ DEFAULT=$(xrandr | grep " connected " | awk '{ print$1 }' | head -n 1)
 if grep -qw "DP-3" <<< "$OUTPUTS"; then
   #echo 'set DP-3 as primary output'
   xrandr --output "DP-3" --primary
-elif grep -q "DP-1" <<< "$OUTPUTS"; then
+elif grep -qw "DP-1" <<< "$OUTPUTS"; then
   #echo 'set DP-1 as primary output'
   xrandr --output "DP-1" --primary
 else
