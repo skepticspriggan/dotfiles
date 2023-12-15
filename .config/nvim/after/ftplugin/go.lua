ba4e8d3ec2a1cb840283037655adafe3f6f3f8be
Skeptic Spriggan <skepticspriggan@protonmail.com>
2023-12-15 12:36:56 +0100

    feat: add errorformat to process go build output

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -2,3 +2,5 @@ vim.opt_local.expandtab = true
 vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
+
+vim.bo.errorformat = [[%f:%l:%c: %m,%-Gmake: *** [%f:%l:%m,%-GFAIL%.%#,%-G#%.%#,%-GPASS%.%#,%-Gok%.%#]]
