736b989de501012b7fe62be4d919338e0d4aad19
Skeptic Spriggan <skepticspriggan@protonmail.com>
2023-12-23 12:14:38 +0100

    correct: fix missing images in export issue

--- .local/bin/markdown-to-pdf.sh
+++ .local/bin/markdown-to-pdf.sh
@@ -3,10 +3,12 @@
 set -euxo pipefail
 
 INPUT_FILEPATH=$1
+INPUT_DIRECTORY=$(dirname "$1")
 OUTPUT_FILEPATH="$EXPORT_DIRECTORY/$(basename "$1").pdf"
 
 pandoc $INPUT_FILEPATH -o $OUTPUT_FILEPATH \
   --pdf-engine=xelatex \
   --bibliography=$NOTES_DIRECTORY/7-source/citations.bib \
   --filter pandoc-citeproc \
-  --csl=$NOTES_DIRECTORY/7-source/acs.csl
+  --csl=$NOTES_DIRECTORY/7-source/acs.csl \
+  --resource-path=$INPUT_DIRECTORY
