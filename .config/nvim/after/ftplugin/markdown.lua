dc6e2ec12afabb5e40979e701b32544fa8558ce0
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-01-09 20:52:30 +0100

    feat: format markdown files

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -2,3 +2,6 @@ vim.opt_local.expandtab = true
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
+vim.opt_local.linebreak = true
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us,nl"
