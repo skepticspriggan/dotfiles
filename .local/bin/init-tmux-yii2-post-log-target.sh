d69a5f4e0c39afa5be85c45bbcab1042c86c1f7b
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-01-18 22:11:41 +0100

    feat: add tmux init scripts

---
+++ .local/bin/init-tmux-yii2-post-log-target.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+. tmux_exists.sh
+. tmux_switch.sh
+
+SESSION="yii2-post-log-target"
+
+if tmux_exists $SESSION; then
+  tmux_switch $SESSION:1.1
+  exit 1
+fi
+
+SESSION_PATH="$REPOS_PATH/yii2-post-log-target"
+tmux -2 new-session -d -s $SESSION -c $SESSION_PATH 
+
+WINDOW="editor"
+
+tmux rename-window -t $SESSION:1 $WINDOW
+sleep 0.3
+tmux send -t $SESSION:$WINDOW 'nvim .' ENTER
+
+WINDOW="commander"
+
+tmux new-window -n $WINDOW -t $SESSION: -c $SESSION_PATH
+tmux split-window -h -c $SESSION_PATH
+
+sleep 0.3
+tmux send -t $SESSION:$WINDOW.2 'git status'
+sleep 0.3
+tmux send -t $SESSION:$WINDOW.3 'composer update'
+
+tmux_switch $SESSION:1.1
