e1a2e2a35449fd379ac743ea4bcb593647433c9d
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-01-27 10:49:26 +0100

    feat: add tmux init scripts

---
+++ .local/bin/init-tmux-yii2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+. tmux_exists.sh
+. tmux_switch.sh
+
+SESSION="yii2"
+
+if tmux_exists $SESSION; then
+  tmux_switch $SESSION:1
+  exit 1
+fi
+
+SESSION_PATH="$REPOS_PATH/yii2"
+tmux -2 new-session -d -s $SESSION -c $SESSION_PATH 
+
+WINDOW="editor"
+
+tmux rename-window -t $SESSION:1 $WINDOW
+sleep 0.3
+tmux send -t $SESSION:$WINDOW 'nvim .' ENTER
+
+WINDOW="commander"
+
+tmux new-window -n $WINDOW -t $SESSION: -c $SESSION_PATH
+sleep 0.3
+tmux send -t $SESSION:$WINDOW.1 'git status'
+
+tmux_switch $SESSION:1
