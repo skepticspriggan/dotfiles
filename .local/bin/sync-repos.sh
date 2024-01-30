ba35dc25db73485591f4983f9e08fe4d8cdcf75c
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-01-30 18:51:19 +0100

    feat: backup some repos coarsely without atomic commits

---
+++ .local/bin/sync-repos.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+for REPO in 'notes' \
+  'notes-custom-pixels'
+do
+  cd $REPOS_PATH/$REPO
+  echo $REPO
+  if [[ $(git status --porcelain) ]]; then
+    echo "Changes"
+    if [ -f ".git/MERGE_HEAD" ]; then
+      echo "Merge conflict"
+    else
+      echo "Sync repo"
+      git add -A
+      git commit -m "add changes by ${HOSTNAME}";
+      git pull
+      git push
+    fi
+  else
+    echo "No changes"
+  fi
+done
