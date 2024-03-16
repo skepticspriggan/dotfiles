0ab42337a82b7d944f3b919a0d10d8a3bd1dadea
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-03-16 14:26:21 +0100

    feat: add key bindings for typing the current date and time

---
+++ .local/bin/type-date.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "/usr/bin/xdotool type $(date '+%Y%m%d')" >/dev/null 2>&1
---
+++ .local/bin/type-time-formatted.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "/usr/bin/xdotool type $(date '+%H:%M:%S')"
