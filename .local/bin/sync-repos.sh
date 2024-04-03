d7b6cd137f9d713a7c50d91ecf9f82a5eccac9a0
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-04-03 22:46:46 +0200

    feat: sync repos

--- .local/bin/sync-repos.sh
+++ .local/bin/sync-repos.sh
@@ -5,7 +5,8 @@ set -euxo pipefail
 echo "sync coarse repos"
 
 for REPO in 'notes' \
-  'notes-custom-pixels'
+  'notes-custom-pixels' \
+  'qmk_firmware'
 do
   cd $REPOS_PATH/$REPO
   echo $REPO
@@ -28,6 +29,54 @@ do
 done
 
 echo "sync fine grained repos" 
+
+for REPO in 'advent-of-code' \
+  'authentication-attempt-limiter' \
+  'authentication-log' \
+  'betaallink' \
+  'bootstrap' \
+  'cmp-reference' \
+  'cv' \
+  'docs' \
+  'file-handler' \
+  'geo-data-finder' \
+  'go-start' \
+  'importer' \
+  'ip-geolocator' \
+  'request-log' \
+  'script-deploy' \
+  'script-rollback' \
+  'tobias' \
+  'tower-of-hanoi' \
+  'update-trusted-ips' \
+  'yii2' \
+  'yii2-coding-standards' \
+  'yii2-post-log-targets'
+do
+  if [ -z "$REPOS_PATH/$REPO" ]; then
+    cd $REPOS_PATH/$REPO
+    echo $REPO
+    if [[ $(git status --porcelain) ]]; then
+      echo "Changes"
+      if [ -f ".git/MERGE_HEAD" ]; then
+        echo "Merge conflict"
+      else
+        echo "Pull repo"
+        git fetch
+        if [ $(git rev-list HEAD...origin/master --count) != 0 ]; then
+          git stash
+          git pull
+          git stash apply
+          git push
+        fi
+      fi
+    else
+      echo "No changes"
+      git pull
+      git push
+    fi
+  fi
+done
 
 cd $HOME
 echo "dotfiles"
