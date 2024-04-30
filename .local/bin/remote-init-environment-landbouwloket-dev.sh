2895368eb4e0a66bb0b9cbe1c264e8f009a7db59
Skeptic Spriggan <skepticspriggan@protonmail.com>
2024-04-30 22:45:36 +0200

    feat: add tmux init scripts

---
+++ .local/bin/remote-init-environment-landbouwloket-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SESSION="landbouwloket-dev"
+
+if [[ -n "$(tmux ls | grep $SESSION)" ]]; then
+  tmux attach -t $SESSION
+  exit 1
+fi
+
+SESSION_PATH="/home/landbouwloket/domains/dev.landbouwloket.nl"
+tmux -2 new-session -d -s $SESSION -c $SESSION_PATH
+
+WINDOW="editor"
+
+tmux rename-window -t $SESSION:0 $WINDOW
+tmux send -t $SESSION:$WINDOW 'cd application; vim .' ENTER
+
+WINDOW="commander"
+
+tmux new-window -n $WINDOW -c $SESSION_PATH
+
+WINDOW="monitor"
+
+tmux new-window -n $WINDOW -c $SESSION_PATH
+tmux send -t $SESSION:$WINDOW 'tail -f -n 200 ./logs/app-frontend.log | grep "error"' ENTER
+
+tmux attach -t $SESSION
